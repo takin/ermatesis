7a8229b80c6206943417eee941f9d22507c287e2
takin <lombok.oc@gmail.com>
2014-02-07 17:42:39 +0700

    model updated

--- app/model/obie_model.php
+++ app/model/obie_model.php
@@ -66,6 +66,12 @@ class Obie_Model extends Database
     	return FALSE;
     }
 
+    public function get_result_occurence_per_iteration( $iterasi )
+    {
+        $sql = "SELECT `left`,`match`,`right` FROM resultoccurence WHERE iterasi=$iterasi";
+        return $this->query($sql);
+    }
+
     public function get_result_occurence( $iterasi )
     {
     	$sql = "SELECT `left`,`match`,`right` FROM resultoccurence WHERE iterasi=$iterasi GROUP BY `left`,`right`";
@@ -86,41 +92,84 @@ class Obie_Model extends Database
      */
     public function insert_result_occurence( $data )
     {
-    	$return = TRUE;
-
     	foreach ( $data as $occurence ) {
     		// build the query
-    		$sql = "INSERT INTO resultoccurence (`no`,`left`,`match`,`right`,`iterasi`) VALUES('o{$occurence['no']}','{$occurence['left']}','{$occurence['match']}','{$occurence['right']}','{$occurence['iterasi']}')";
+    		$sql = "INSERT INTO resultoccurence (`no`,`left`,`match`,`right`,`iterasi`) VALUES('{$occurence['no']}','{$occurence['left']}','{$occurence['match']}','{$occurence['right']}','{$occurence['iterasi']}')";
 
     		// run the query parameter FALSE berfungsi untuk mencegah method query untuk melakukan 
     		// auto fetching data ( karena ini adalah proses insert data )
-    		$query_ok = $this->query($sql, FALSE);
+    		$ok = $this->query($sql, FALSE);
     		// jika masing-masing query berhasil, maka set $resturn menjadi TRUE
-    		if( $query_ok ) {
-    			$return = TRUE;
-    		}
-    		else {
+    		if( ! $ok ) {
     			// jika query gagal, maka hentikan proses iterasi insert data dan set $resturn menjadi FALSE
     			$return = FALSE;
     			break;
     		}
     	}
     	
-    	return $return;
+    	return TRUE;
     }
 
     public function insert_result_pattern( $data )
     {
     	$return = TRUE;
     	foreach ($data as $i => $res) {
-    		$sql = "INSERT INTO resultpattern (`no`,`left`,`right`,`Fi`,`Ni`,`RlogF`,`iterasi`) VALUES('p{$res['no']}','$res[left]','$res[right]','$res[fi]','$res[ni]','$res[Rlog]','$res[iterasi]')";
-    		$ok = $this->query($sql, FALSE);
-    		if( ! $ok ){
-    			$return = FALSE;
-    			break;
-    		}
-    	}
-    	return $return;
+            $sql = "INSERT INTO resultpattern (`no`,`left`,`right`,`Fi`,`Ni`,`RlogF`,`iterasi`) VALUES('{$res['no']}','$res[left]','$res[right]','$res[Fi]','$res[Ni]','$res[RlogF]','$res[iterasi]')";
+            $ok = $this->query($sql, FALSE);
+            if( ! $ok ){
+                $return = FALSE;
+                break;
+            }
+        }
+        return $return;
+    }
+
+    public function get_result_pattern( $iterasi )
+    {
+        $sql = "SELECT * FROM resultpattern WHERE iterasi = $iterasi";
+        return $this->query($sql);
+    }
+
+    public function get_patternset_for_set_of_extraction( $dataset )
+    {
+        $set_of_extraction = array();
+        $dataset = $this->get_result_pattern(1)['rows'];
+        foreach ($dataset as $i => $data) {
+            $sql = "SELECT LCASE(`match`) AS `match`,`left`,`right` FROM patternset WHERE `left`='$data[left]' AND `right`='$data[right]' GROUP BY `match`";
+            $set_of_extraction[] = $this->query($sql)['rows'];
+        }
+        return $set_of_extraction;
+    }
+
+    public function insert_score_extraction( $score_extraction_data )
+    {
+        foreach ($score_extraction_data as $i => $data) {
+            $sql = "INSERT INTO skorekstraksi(`match`,`no`,`RlogF`,`iterasi`) VALUES('$data[match]','$data[no]','$data[RlogF]','$data[iter]')";
+            $ok = $this->query($sql, FALSE);
+            if( ! $ok ) {
+                return FALSE;
+                break;
+            }
+        }
+        return TRUE;
+    }
+
+    public function compute_best_extraction()
+    {
+        # code...
+    }
+
+    public function insert_best_extraction( $best_extraction_data )
+    {
+        foreach ($best_extraction_data as $i => $data) {
+            $sql = "INSERT INTO bestextraction(`match`,`score`,`iterasi`) VALUES('$data[match]','$data[jum]','$data[iter]')";
+            $ok = $this->query( $sql, FALSE );
+            if( ! $ok ) {
+                return FALSE;
+                break;
+            }
+        }
+        return TRUE;
     }
 
 }
